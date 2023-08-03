de125a11ca56fa33d1eefc79771b8ebf834a0b12
Garik-create <pfdfkbybujhm@yandex.ru>
2023-08-03 12:13:56 +0300

    Initial commit

---
+++ src/test/java/com/skypro/home/HomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
